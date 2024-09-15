00809e489addc31740730fb2df4a06abceb4d404
Hernán Leandro Bailo <hernanbailo@hotmail.com>
2024-09-15 02:32:52 -0300

    Add basic control functionality

--- src/SolenoidValveCtrlMod.cpp
+++ src/SolenoidValveCtrlMod.cpp
@@ -1,41 +1,53 @@
 #include "SolenoidValveCtrlMod.h"
 
 
-bool SolenoidValveCtrlMod::execute(bool manual_on_cmd, bool manual_off_cmd) noexcept
+/** Initializes the control module with the passed digital output pin. */
+SolenoidValveCtrlMod::SolenoidValveCtrlMod(PinName on_actuating_signal_pin)
+    : on_actuating_signal(on_actuating_signal_pin)
+{
+  
+}
+
+
+void SolenoidValveCtrlMod::execute(const SolenoidValveCtrlMod::Commands & cmds) noexcept
 {
 
-  // On actuating signal computing
-  if (protection_off_cmd) {
-    on_actuating_signal = OFF ;
-    
-  } else if (protection_on_cmd) {
-    on_actuating_signal = ON ;
-    
-  } else if (enabling_off_cmd) {
-
-    if ((manual_off_cmd and mode == MANUAL) or (auto_off_cmd and mode == AUTO)) {
-      on_actuating_signal = OFF ;
-    }
-
-  } else if (enabling_on_cmd) {
-      
-    if ((manual_on_cmd and mode == MANUAL) or (auto_on_cmd and mode == AUTO)) {
-      on_actuating_signal = ON ;
-    }
-      
+  // Mode commands processing
+  if (cmds.manual_mode_cmd) {
+    mode = MANUAL ;
+  } else if (cmds.auto_mode_cmd) {
+    mode = AUTO ;
   }
   
-  // Mode commands processing
-  if (manual_mode_cmd) {
-    mode = MANUAL ;
-  } else if (auto_mode_cmd) {
-    mode = AUTO ;
+  // Control commands processing & ON actuating signal computing
+  if (cmds.protection_off_cmd) {
+    on_actuating_signal = OFF ;
+    state = OFF ;
+    
+  } else if (cmds.protection_on_cmd) {
+    on_actuating_signal = ON ;
+    state = ON ;
+    
+  } else if (cmds.enabling_off_cmd) {
+
+    if ((cmds.manual_off_cmd and mode == MANUAL) or (cmds.auto_off_cmd and mode == AUTO)) {
+      on_actuating_signal = OFF ;
+      state = OFF ;
+    }
+
+  } else if (cmds.enabling_on_cmd) {
+      
+    if ((cmds.manual_on_cmd and mode == MANUAL) or (cmds.auto_on_cmd and mode == AUTO)) {
+      on_actuating_signal = ON ;
+      state = ON ;
+    }
+
   }
   
 }
 
 
-bool SolenoidValveCtrlMod::isOn() noexcept
+bool SolenoidValveCtrlMod::isOn() const noexcept
 {
 
   return state == ON ;
@@ -43,7 +55,7 @@ bool SolenoidValveCtrlMod::isOn() noexcept
 }
 
 
-bool SolenoidValveCtrlMod::isOff() noexcept
+bool SolenoidValveCtrlMod::isOff() const noexcept
 {
 
   return state == OFF ;
