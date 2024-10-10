3b83a4c20963e5c4e19686965a3254399db98a41
Hernán Leandro Bailo <hernanbailo@hotmail.com>
2024-10-10 00:40:33 -0300

    Fix

--- src/SolenoidValve.cpp
+++ src/SolenoidValve.cpp
@@ -5,8 +5,8 @@
  *  @param on_actuating_signal_pin the actuating signal DO pin.
  *  @param response_time the solenoid valve response time.
  */
-SolenoidValve::SolenoidValve(PinName on_actuating_signal_pin, std::chrono::milliseconds response_time = 50ms)
-  : on_actuating_signal(on_actuating_signal_pin, false), response_time(response_time), state(OFF)
+SolenoidValve::SolenoidValve(PinName on_actuating_signal_pin, std::chrono::milliseconds response_time)
+  : on_actuating_signal(on_actuating_signal_pin, false), response_time(response_time), state(State::OFF)
 {
   
 }
@@ -17,26 +17,26 @@ SolenoidValve::State SolenoidValve::on()
 
   switch (state) {
     
-  case OFF: 
+  case State::OFF: 
     on_actuating_signal = true ;
     timer.start() ;    
-    state = TURNING_ON ;
+    state = State::TURNING_ON ;
     break ;
 
-  case ON:
+  case State::ON:
     break ;
     
-  case TURNING_OFF:
+  case State::TURNING_OFF:
     on_actuating_signal = true ;
     timer.reset() ;
-    state = TURNING_ON ;
+    state = State::TURNING_ON ;
     break ;
     
-  case TURNING_ON:
+  case State::TURNING_ON:
     
     if (duration_cast<milliseconds>(timer.elapsed_time()).count() > response_time) {
       timer.stop() ;
-      state = ON ;
+      state = State::ON ;
     }
     
     break ;
@@ -53,28 +53,28 @@ SolenoidValve::State SolenoidValve::off()
 
   switch (state) {
 
-  case OFF:
+  case State::OFF:
     break ;
     
-  case ON:
+  case State::ON:
     on_actuating_signal = false ;
     timer.start() ;
-    state = TURNING_OFF ;
+    state = State::TURNING_OFF ;
     break ;
     
-  case TURNING_OFF:
+  case State::TURNING_OFF:
 
     if (duration_cast<milliseconds>(timer.elapsed_time()).count() > response_time) {
       timer.stop() ;
-      state = OFF ;
+      state = State::OFF ;
     }
     
     break ;
     
-  case TURNING_ON:
+  case State::TURNING_ON:
     on_actuating_signal = false ;
     timer.reset() ;
-    state = TURNING_OFF ;
+    state = State::TURNING_OFF ;
     break ;
     
   }
