08772786a2b19a379ed5dac2d15d7d06e8ffec3a
Hernán Leandro Bailo <hernanbailo@hotmail.com>
2024-12-04 19:31:13 -0300

    Improve Siren documentation

--- include/Siren.h
+++ include/Siren.h
@@ -3,10 +3,17 @@
 
 #include "mbed.h"
 
+/**
+ * @brief A class to control a siren using a PWM output signal.
+ * 
+ * This class allows for controlling the activation and deactivation of a siren
+ * as well as setting its tone frequency. It uses the mbed `PwmOut` class
+ * to generate the required PWM signal.
+ */
 class Siren {
 
 public:
-  /** @brief Initializes the siren. */
+  /** @brief Constructs a Siren object and initializes the PWM signal. */
   Siren(PinName on_actuating_signal_pin, float tone_frequency);
 
   /** @brief Activates the siren. */
